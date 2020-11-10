eace1a522c038b90872c08da9dd2c4a2485fe2ef
Logan Arnett <logan@loganarnett.com>
2020-11-09 21:26:48 -0500

    updating for styles

--- src/components/videochat/Classroom/index.tsx
+++ src/components/videochat/Classroom/index.tsx
@@ -14,7 +14,7 @@ import {
   RemoteVideo,
   useContentShareControls
 } from 'amazon-chime-sdk-component-library-react'
-import { Drawer, makeStyles, Tab, Tabs, Toolbar } from '@material-ui/core'
+import { Box, Drawer, makeStyles, Tab, Tabs, Toolbar } from '@material-ui/core'
 import { Modal } from '@mvrk-hq/vx360-components'
 
 import MeetingControls from '../MeetingControls'
@@ -33,6 +33,8 @@ import { onUpdateSession } from 'graphql/subscriptions'
 import { ISubscriptionObject, ISession } from 'types'
 
 import { ReactComponent as Logo } from 'assets/verizon-logo.svg'
+import { ConditionalWrapper } from 'components/shared'
+import { NullEngine } from 'babylonjs'
 
 interface ClassRoomVideoChatModalProps {}
 
@@ -181,21 +183,32 @@ export const ClassRoomVideoChatModal: FC<ClassRoomVideoChatModalProps> = () => {
                   <MeetingDetails isClassroom={true} />
                   {isLocalUserSharing || sharingAttendeeId ? <ContentShare /> : null}
                   <VideoGrid
-                    layout='standard'
+                    layout={null}
                     size={isVideoEnabled ? tiles.length + 1 : tiles.length}
                     style={isVideoEnabled || tiles.length > 0 ? {} : { backgroundColor: 'transparent' }}
                   >
-                    {isVideoEnabled ? <LocalVideo nameplate={`${user?.firstName} ${user?.lastName}`} /> : null}
-                    {tiles.map(tileId => {
-                      const attendeeId = tileIdToAttendeeId[tileId]
-                      return (
-                        <RemoteVideo
-                          tileId={tileId}
-                          name={roster[attendeeId] && roster[attendeeId].name ? roster[attendeeId].name : ''}
-                          style={{ border: '1px solid grey', gridArea: '' }}
-                        />
-                      )
-                    })}
+                    <ConditionalWrapper
+                      condition={isLocalUserSharing || sharingAttendeeId}
+                      wrapper={children => (
+                        <Box display='flex' flexDirection='column'>
+                          {children}
+                        </Box>
+                      )}
+                    >
+                      <>
+                        {isVideoEnabled ? <LocalVideo nameplate={`${user?.firstName} ${user?.lastName}`} /> : null}
+                        {tiles.map(tileId => {
+                          const attendeeId = tileIdToAttendeeId[tileId]
+                          return (
+                            <RemoteVideo
+                              tileId={tileId}
+                              name={roster[attendeeId] && roster[attendeeId].name ? roster[attendeeId].name : ''}
+                              style={{ border: '1px solid grey', gridArea: '' }}
+                            />
+                          )
+                        })}
+                      </>
+                    </ConditionalWrapper>
                   </VideoGrid>
                   <MeetingControls
                     setVisible={setVisible}
