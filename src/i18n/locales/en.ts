b464690259401a18ad574433f480dc581c0ab5de
Fortunes <fortunes.technology@gmail.com>
2020-11-11 14:31:01 +0000

    Update session page description

--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -139,7 +139,7 @@ const scenes: IScenesDict = {
     'What can Verizon 5G Ultra Wideband do for your business? Explore the various ways our 5G network is moving businesses like yours forward.',
   sessionsSceneTitle: 'Talk with our experts.',
   sessionsSceneBlurb:
-    'Hear from thought-provoking leaders who explain how Verizon 5G is already transforming industries.',
+    'Learn how your business can take advantage of the full, transformative power of Verizon 5G Ultra Wideband. Let our experts to take you on a deeper dive into vertical-specific use cases to demonstrate how Verizon 5G can benefit you. Each session includes a live Q&A. Capacity is limited to 200 attendees each.',
   clickAndDragToExplore: 'Click and drag to explore',
   theEventWillBeginIn: 'The next session will begin in',
   theEventWillStartOn: 'The next session will start on',
