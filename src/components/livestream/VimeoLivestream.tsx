f8558518b27890660e92f8097db69bfeff9002c1
suraj pratap <surajpratap@icloud.com>
2020-11-13 19:49:19 +0530

    events close

--- src/components/livestream/VimeoLivestream.tsx
+++ src/components/livestream/VimeoLivestream.tsx
@@ -1,10 +1,11 @@
 import React, { FC, useEffect, useState } from 'react'
-import { IconButton, makeStyles } from '@material-ui/core'
+import { Box, IconButton, makeStyles, Typography } from '@material-ui/core'
 import { Close } from '@material-ui/icons'
-import { RouteTransition } from 'components'
+import { PillButton, RouteTransition } from 'components'
 import { graphQLMutation } from '../../graphql/helpers'
 import { createUserInteraction } from '../../graphql/mutations'
 import { EventStages, IUser } from '../../types'
+import { useHistory } from 'react-router-dom'
 
 interface VimeoLiveStreamProps {
   useBackupStream: Boolean
@@ -15,6 +16,8 @@ interface VimeoLiveStreamProps {
 export const VimeoLiveStream: FC<VimeoLiveStreamProps> = ({ useBackupStream, user, eventStage }) => {
   const classes = useStyles()
   const [redirectTrigger, setRedirectTrigger] = useState<boolean>(false)
+  const [openModal, setOpenModal] = useState(false)
+  const history = useHistory()
 
   useEffect(() => {
     console.log(user)
@@ -39,7 +42,7 @@ export const VimeoLiveStream: FC<VimeoLiveStreamProps> = ({ useBackupStream, use
       <div className={classes.root}>
         <IconButton
           className={classes.closeButton}
-          onClick={() => setRedirectTrigger(true)}
+          onClick={() => setOpenModal(true)}
           disableFocusRipple
           disableRipple
           disableTouchRipple
@@ -69,6 +72,41 @@ export const VimeoLiveStream: FC<VimeoLiveStreamProps> = ({ useBackupStream, use
         )}
       </div>
       <RouteTransition animationTrigger={redirectTrigger} route='/event' timeout={300} />
+      {openModal && (
+        <div className={classes.modal}>
+          <div className={classes.modalBody}>
+            <Box p={4}>
+              <Box mb={4} mt={2}>
+                <Typography style={{ color: '#000' }} variant='h4'>
+                  Join a breakout session.
+                </Typography>
+              </Box>
+              <Typography style={{ color: '#000' }}>
+                It’s almost time to hear how your business can take advantage of the full, transformative power of
+                Verizon 5G Ultra Wideband. Let our experts to take you on a deeper dive into vertical-specific use cases
+                to demonstrate how Verizon 5G can benefit you. Each session includes a live Q&A. Click below to see
+                which sessions are available to join.
+              </Typography>
+              <Box>
+                <PillButton
+                  type='button'
+                  className='button'
+                  variant='outlined'
+                  textColor='white'
+                  backgroundColor='black'
+                  onClick={() => history.push('/event')}
+                  classes={{ root: classes.toastESSButton }}
+                >
+                  Pick your breakout session
+                </PillButton>
+              </Box>
+            </Box>
+            <IconButton className={classes.closeButtonModal} onClick={() => setRedirectTrigger(true)}>
+              <Close />
+            </IconButton>
+          </div>
+        </div>
+      )}
     </>
   )
 }
@@ -94,6 +132,42 @@ const useStyles = makeStyles(() => ({
     zIndex: 10,
     '& svg': {
       color: '#fff'
+    }
+  },
+  modal: {
+    position: 'fixed',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    zIndex: 9999,
+    height: '100vh',
+    width: '100vw',
+    top: 0,
+    left: 0
+  },
+  modalBody: {
+    backgroundColor: '#fff',
+    width: 560,
+    position: 'relative'
+  },
+  closeButtonModal: {
+    position: 'absolute',
+    right: 0,
+    top: 0
+  },
+  toastESSButton: {
+    height: '24px',
+    fontSize: '12px',
+    marginBottom: '1rem',
+    padding: '0 20px',
+    display: 'flex',
+    flexWrap: 'nowrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    '&:hover': {
+      backgroundColor: 'black',
+      color: 'white'
     }
   }
 }))
