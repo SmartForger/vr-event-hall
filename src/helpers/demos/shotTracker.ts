5ae3f1aba0f0552a9c136fb9b62d10fece80e64c
MVRK-Matheux <70237520+MVRK-Matheux@users.noreply.github.com>
2020-11-15 16:00:48 -0500

    Update shotTracker.ts

--- src/helpers/demos/shotTracker.ts
+++ src/helpers/demos/shotTracker.ts
@@ -6,8 +6,8 @@ export const shotTracker: IDemo = {
   video: 'shottracker720.mp4',
   conversationId: '9218554c-0bc0-4e61-b6f8-0fef42d984a1',
   end: {
-    header: 'The future is here.',
-    body: 'Learn how 5G and MEC can help breathe life into organizations’ more daring ideas.',
+    header: '5G Edge and data-driven decision making.',
+    body: '',
     nextDemoText: 'The transformation of healthcare',
     nextDemo: E3DDemoNameVals.avesha,
     nextDemoThumbnail: 'avesha-thumbnail.png',
